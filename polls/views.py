88bd3f6959b82aace9d1fbb9b97e3657907c8d2a
Rockerzega <rockerzega@gmail.com>
2021-02-11 20:00:17 -0500

    Cuarto COmmit

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect #, Http404
 #from django.template import RequestContext, loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
@@ -9,21 +10,38 @@ from .models import Choice, Question
 #def index(request):
 #    return HttpResponse("Hola. Estas en el incio de la página.")
 
-def detail(request, question_id):
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ Retorna las ultimas cinco preguntas publicadas. """
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+"""def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
     #return HttpResponse("Estas mirando la pregunta %s." % question_id)
 
-"""def detail(request, question_id):
+def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})"""
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+"""
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
@@ -44,7 +62,8 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 #view que permite ver las ultimas 5 publicaciones sepradas con una coma 
-def index(request):
+"""def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+"""
